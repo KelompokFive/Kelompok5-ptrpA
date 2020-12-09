f07a74a9c165c89feb95fd77f201f0adff8ddebb
KelompokFive <75669605+KelompokFive@users.noreply.github.com>
2020-12-10 00:14:19 +0700

    Add files via upload

---
+++ data/titikacuan_1.js
@@ -0,0 +1,1 @@
+var json_titikacuan_1 = {"type":"FeatureCollection","name":"titikacuan_1","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"id":"1"},"geometry":{"type":"Point","coordinates":[109.139646799844513,-6.974160821697831]}}]}
